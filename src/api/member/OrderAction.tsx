dea835f518e053275694745b067dea99c5f031cf
tierkato <kato@tierline.com>
2020-12-16 13:54:46 +0900

    tweak

--- src/api/member/OrderAction.tsx
+++ src/api/member/OrderAction.tsx
@@ -5,10 +5,10 @@ export default class OrderAction {
 
   static mappingUrl = "/api/member/order/"
 
-  static async save() {
+  static async save(data: any) {
     const url = UrlGenerator(this.mappingUrl, 'save')
     console.log('url:' + url)
-    const res = await axios.post(url);
+    const res = await axios.post(url, data);
     console.log(res)
     return res.data
   };
--- src/components/form/OrderForm.tsx
+++ src/components/form/OrderForm.tsx
@@ -1,26 +1,97 @@
 import React from 'react';
-import { Content, Form, Input, Item, Label, Text, Button } from 'native-base';
+import { Content, Form, Input, Item, Label, Text, View } from 'native-base';
 import { useForm, Controller } from 'react-hook-form'
-import { TextInput } from 'react-native';
+import { StyleSheet, TextInput, Button } from 'react-native';
 import OrderAction from '../../api/member/OrderAction'
 
 const OrderForm = () => {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data: any) => console.log(data);
-
-  console.log(watch("example"));
+  const { control, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data: any) => OrderAction.save(data);
 
   return (
-    <Content>
-      <Form>
-        <form onSubmit={handleSubmit(onSubmit)}></form>
-        <Label>名前</Label>
-        <Input placeholder="岡本 太郎" />
-      </Form>
-      <Button onPress={() => OrderAction.save()}>
-        <Text>注文を確定する</Text>
-      </Button>
-    </Content >
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View
+        style={styles.view}>
+        <View style={{ flex: 0.2, alignItems: 'flex-end' }}>
+          <Text>名前</Text>
+        </View>
+        <View style={{ flex: 0.8 }}>
+          <Controller
+            control={control}
+            render={({ onChange, onBlur, value }) => (
+              <TextInput
+                style={styles.textInput}
+                placeholder="お名前"
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+              />
+            )}
+            name="name"
+            rules={{
+              required: true,
+              maxLength: 10,
+            }}
+            defaultValue=""
+          />
+          {errors.name && errors.name.type === 'required' && (
+            <Text style={{ color: 'red' }}>Nameは必須です。</Text>
+          )}
+          {errors.name && errors.name.type === 'maxLength' && (
+            <Text style={{ color: 'red' }}>
+              Nameは10文字以内で入力してください。
+            </Text>
+          )}
+        </View>
+      </View>
+      <View
+        style={styles.view}>
+        <View style={{ flex: 0.2, alignItems: 'flex-end' }}>
+          <Text>郵便番号</Text>
+        </View>
+        <View style={{ flex: 0.8 }}>
+          <Controller
+            control={control}
+            render={({ onChange, onBlur, value }) => (
+              <TextInput
+                style={styles.textInput}
+                placeholder="000-0000"
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+              />
+            )}
+            name="zipCode"
+            rules={{
+              pattern: /^\d{3}-\d{4}$/,
+            }}
+            defaultValue=""
+          />
+          {errors.zipCode && errors.zipCode.type === 'pattern' && (
+            <Text style={{ color: 'red' }}>
+              郵便番号のフォーマットが不正です。
+            </Text>
+          )}
+        </View>
+      </View>
+      <Button title="注文を確定する" onPress={handleSubmit(onSubmit)} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: '80%',
+    fontSize: 20,
+    margin: '4%',
+  }
+});
+
 export default OrderForm;
