b855f8cf8dedef3169d699478872b0fe8b19a3a8
tierkato <kato@tierline.com>
2021-01-20 09:24:35 +0900

    カートの無限ループ解消

--- src/components/list/cart/CartItemList.tsx
+++ src/components/list/cart/CartItemList.tsx
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardItem, Text, Right, Button, H3 } from 'native-base';
+import { Card, CardItem, Text, Right, Button, H3, View } from 'native-base';
 import { FlatList, Image, StyleSheet } from 'react-native';
 import UrlApi from '../../../api/UrlApi';
 import { flashMessage } from '../../flashMessage/FlashMessage';
+import { useNavigation } from '@react-navigation/native';
 import Api from '../../../api/Api';
 import { CartItem } from '../../../domain/CartItem';
+import Storage from '../../../Storage';
+import LargeButton from '../../../components/button/LargeButton';
 
 // TOREVIEW : リストもコンポーネントに切り出したい
-// 個別の画面はほとんど共通化できない。
-// screenへ
 const CartItemList = () => {
+  const navigation = useNavigation();
   const [cartItems, setItems] = useState([]);
+  const [count, setCount] = useState(0);
 
-  // const callBack = (res: any, mounted: boolean) => {
-  //   if (mounted) {
-  //     setItems(res.items);
-  //   }
-  // };
+  const navi = () => {
+    navigation.navigate('OrderForm');
+  };
+
+  const callBack = (res: any) => {
+    Storage.setCart(res.items);
+    setItems(res.items);
+  };
 
   useEffect(() => {
-    Api.get('/api/member/cart/', setItems);
-  }, [cartItems]);
+    Api.get('/api/member/cart/', callBack);
+  }, [count]);
 
-  console.log('mugen');
+  const removeParticularProduct = async (
+    productId: number,
+    productName: string,
+  ) => {
+    await Api.post(`/api/member/cart/remove/${productId}`, setItems);
+    flashMessage(productName, '削除しました', 500, 'red');
+    setCount(count + 1);
+  };
 
   //   // clean up関数(必要になったら追加する)
   // useEffect(() => {
@@ -32,11 +45,6 @@ const CartItemList = () => {
   //   mounted = false;
   // };
   // }, [cartItems]);  const [orderedItems, setOrderedItem] = useState([]);
-
-  const removeParticularProduct = (productId: number, productName: string) => {
-    Api.post(`/api/member/cart/remove/${productId}`, setItems);
-    flashMessage(productName, '削除しました', 500, 'red');
-  };
 
   const renderItem = ({ item }: { item: CartItem }) => {
     return (
@@ -66,13 +74,19 @@ const CartItemList = () => {
     );
   };
 
-  return (
-    // <Text>{cartItems}</Text>
-    <FlatList
-      data={cartItems}
-      renderItem={renderItem}
-      keyExtractor={(item, index) => index.toString()}
-    />
+  return cartItems.length > 0 ? (
+    <View style={styles.body}>
+      <FlatList
+        data={cartItems}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <LargeButton text={'注文処理へ進む'} onPress={navi} />
+    </View>
+  ) : (
+    <View style={styles.body}>
+      <H3>カートに商品がありません</H3>
+    </View>
   );
 };
 
@@ -81,6 +95,9 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     width: 100,
     height: 100,
+  },
+  body: {
+    flex: 8,
   },
 });
 
--- src/screen/cart/CartScreen.tsx
+++ src/screen/cart/CartScreen.tsx
@@ -1,52 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { H2 } from 'native-base';
-import NavBarBottom from '../../components/nav/NavBarBottom';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { View } from 'native-base';
 import CartItemList from '../../components/list/cart/CartItemList';
-import Api from '../../api/Api';
-import LargeButton from '../../components/button/LargeButton';
+import NavBarBottom from '../../components/nav/NavBarBottom';
 
 const CartScreen = () => {
-  const navigation = useNavigation();
-  const [hasItem, setHasItem] = useState(false);
-
-  useEffect(() => {
-    let unmounted = false;
-    if (!unmounted) {
-      Api.get('/api/member/cart/hasItem', setHasItem);
-    }
-    return () => {
-      unmounted = true;
-    };
-  }, []);
-
-  const navi = () => {
-    navigation.navigate('OrderForm');
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.itemList}>
-        <CartItemList setHasItem={setHasItem} />
-      </View>
-      {hasItem ? (
-        <LargeButton text={'注文処理へ進む'} onPress={navi} /> // アイコン
-      ) : (
-        <View style={styles.message}>
-          <H2>カートに商品がありません</H2>
-        </View>
-      )}
+      <CartItemList />
       <NavBarBottom />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  itemList: {
-    flex: 8,
   },
   message: {
     position: 'absolute',
@@ -54,4 +23,5 @@ const styles = StyleSheet.create({
     left: 10,
   },
 });
+
 export default CartScreen;
